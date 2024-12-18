56e0a2d6d454241942bfd5cbead1619ff3615818
mm011106 <mm011106@gmail.com>
2024-12-18 10:50:11 +0900

    add sample ID file

---
+++ Arduino/AtomS3_HTTP_POST_test/SAMPLE_connection_IDs.h
@@ -0,0 +1,9 @@
+#ifndef CONNECTION_IDS_H
+#define CONNECTION_IDS_H
+
+const char* SSID = "ssid";
+const char* PASSWORD = "wifi passowrd";
+
+const char* API_URL = "GAS API URL"
+
+#endif // CONNECTION_IDS_H
