c196eef3ba6bce48fd29cd3260364fe19471d2cb
mm011106 <mm011106@gmail.com>
2025-01-14 18:43:23 +0900

    add SENSOR_ID

--- Arduino/AtomS3_HTTP_POST_test/SAMPLE_connection_IDs.h
+++ Arduino/AtomS3_HTTP_POST_test/SAMPLE_connection_IDs.h
@@ -4,6 +4,8 @@
 const char* SSID = "ssid";
 const char* PASSWORD = "wifi passowrd";
 
-const char* API_URL = "GAS API URL"
+const char* API_URL = "GAS API URL";
+
+const char* SENSOR_ID = "sensor1";
 
 #endif // CONNECTION_IDS_H
